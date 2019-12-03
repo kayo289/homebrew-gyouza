3ce9ba59dfc02bf667a7390b522f8347506dd0d6
kayo289 <kayokapi@icloud.com>
2019-12-04 01:40:15 +0900

    gyouza

--- gyouza.rb
+++ gyouza.rb
@@ -6,7 +6,7 @@ class Gyouza < Formula
   homepage "https://github.com/kayo289/gyouza"
   head "https://github.com/kayo289/gyouza.git"
   url "https://github.com/kayo289/gyouza/archive/0.0.1.tar.gz"
-  sha256 = "40a6743e55b9ed4e8f6a53578d7c024e70339f15cf3431ad3e10385a738d1348"
+  sha256 "40a6743e55b9ed4e8f6a53578d7c024e70339f15cf3431ad3e10385a738d1348"
   head "https://github.com/kayo289/gyouza.git", :tag => "0.0.1"
 
   def install

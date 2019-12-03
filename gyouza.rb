487f234c88b66cfce05cc7af1ddfc70128359c2e
kayo289 <kayokapi@icloud.com>
2019-12-04 00:59:25 +0900

    second

--- gyouza.rb
+++ gyouza.rb
@@ -5,9 +5,11 @@ class Gyouza < Formula
   desc ""
   homepage ""
   head "https://github.com/kayo289/gyouza.git"
+  url "https://github.com/kayo289/gyouza/archive/0.0.1.tar.gz"
+  sha256 = 40a6743e55b9ed4e8f6a53578d7c024e70339f15cf3431ad3e10385a738d1348
+  head "https://github.com/kayo289/gyouza.git", :tag => 0.0.1
+# depends_on "cmake" => :build
 
-  # depends_on "cmake" => :build
- 
   def install
     bin.install "bin/gyouza"
   end# system "cmake", ".", *std_cmake_args
